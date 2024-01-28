9644222b8df2b7166a13692aea4c33808bae006a
Alisher <93440110+AlisherSrc@users.noreply.github.com>
2024-01-28 20:48:13 +0600

    adding user model and api for creating users

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require('./models/User');
 const express = require("express");
 const app = express();
 
-app.get('/test',(req,res) => {
-    res.json("test ok");
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://blog:yIt8sO9k8UTVzXjB@cluster0.cz99cmu.mongodb.net/?retryWrites=true&w=majority')
+
+app.post('/register', async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.create({username,password});
+    res.json(userDoc);
 })
 
 
--- client/src/pages/register/index.jsx
+++ client/src/pages/register/index.jsx
@@ -6,6 +6,15 @@ const Register = () => {
     const [username,setUsername] = useState('');
     const [password,setPassword] = useState('');
 
+    const register = async (e) => {
+        e.preventDefault();
+
+        await fetch("http://localhost:4000/register",{
+            method: "POST",
+            body: JSON.stringify({username,password}),
+            headers: {"Content-Type":"application/json"},
+        })
+    }
 
     return (
         <div className={`${styles.register}`}>
@@ -13,7 +22,7 @@ const Register = () => {
                 <h1>Register</h1>
                 <input type='text' placeholder='username' onChange={(e) => setUsername(e.target.value)}/>
                 <input type='password' placeholder='password' onChange={(e) => setPassword(e.target.value)}/>
-                <button>register</button>
+                <button onClick={(e) => register(e)}>register</button>
             </form>
         </div>
     )
