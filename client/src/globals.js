4a8122e5afdf40362c9ec6345ab2b8f13a140272
Alisher <93440110+AlisherSrc@users.noreply.github.com>
2024-05-04 12:01:41 +0500

    host change

--- client/src/globals.js
+++ client/src/globals.js
@@ -1,2 +1,2 @@
-// export const HOST = "https://alisher-sk.onrender.com"
-export const HOST = "http://localhost:4000"
+export const HOST = "https://alisher-sk.onrender.com"
+// export const HOST = "http://localhost:4000"
